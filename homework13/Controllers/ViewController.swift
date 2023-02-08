aa9b76e39eb8e44b53c16bc92bbe12ee8534d4cb
mi1212 <mc_gorba4ev@mail.ru>
2023-02-08 19:59:26 +0300

    next commit

---
+++ homework13/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework13
+//
+//  Created by Mikhail Chuparnov on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
